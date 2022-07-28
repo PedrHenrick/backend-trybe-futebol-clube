c8abbefec39b9705f40f992468af269c726aada9
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-28 16:31:37 -0300

    feat(model): :sparkles: Criando model para a tabela users

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,43 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
+
+export default User;
