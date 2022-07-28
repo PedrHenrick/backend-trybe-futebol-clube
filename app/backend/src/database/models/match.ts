67e91be3d3eb4177479c606597775c08fed1b25f
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-28 17:22:51 -0300

    feat(model): :sparkles: Criando model da tabela matches

---
+++ app/backend/src/database/models/match.ts
@@ -0,0 +1,48 @@
+import { INTEGER, Model } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public awayTeamGoals!: number;
+  public inProgress!: number;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    unique: true,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'match',
+  timestamps: false,
+});
+
+export default Match;
