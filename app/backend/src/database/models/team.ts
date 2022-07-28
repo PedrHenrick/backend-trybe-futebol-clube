c8f67c65533a4c6ef9c5cc9b6e3f25a8515076e8
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-28 17:23:36 -0300

    feat(model): :sparkles: Criando model da tabela team

---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,27 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  public id!: number;
+  public teamName!: number;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'team',
+  timestamps: false,
+});
+
+export default Team;
