3ece87e58f29910f03eb19df87207b1054ce8cc9
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-29 17:10:39 -0300

    feat(mock): :sparkles: Adicionando arquivo de mocks para a rota /login

---
+++ app/backend/src/tests/mocks/login.mock.ts
@@ -0,0 +1,10 @@
+export const loginSuccessful = {
+  username: 'teste',
+  role: 'user',
+  email: 'teste@teste.com',
+  password: 'senhaDeTesteCorreta'
+}
+
+export const loginValidate = {
+  role: 'user',
+}
