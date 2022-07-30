1d6d27d06119862bedb9cb8f5811bf5a59d4ddfe
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-30 18:44:20 -0300

    perf(test): :zap: Adicionando describes para os testes e alterando verificações

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -15,119 +15,141 @@ const { expect } = chai;
 
 describe('Rota /login', () => {
   let chaiHttpResponse: Response;
+  describe('Passando os dados corretos', () => {
+    before(async () => {
+      sinon
+        .stub(user, "findOne")
+        .resolves(loginSuccessful as user);
+    });
 
-  before(async () => {
-    sinon
-      .stub(user, "findOne")
-      .resolves(loginSuccessful as user);
+    after(()=>{ (user.findOne as sinon.SinonStub).restore() });
+
+    it('Testando se é possível fazer login na plataforma', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: 'admin@admin.com',
+          password: 'secret_admin'
+        });
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.an('object');
+      expect(chaiHttpResponse.body.token).to.be.an('string');
+    });
   });
 
-  after(()=>{
-    (user.findOne as sinon.SinonStub).restore();
-  });
+  describe('Faltando dados', () => {
+    it('Testando se não é possível fazer login sem o email', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          password: 'secret_admin'
+        });
 
-  it('Testando se é possível fazer login na plataforma', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({
-        email: 'teste@teste.com',
-        password: 'senhaDeTesteCorreta'
-      });
-
-    expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.an('object');
-    expect(chaiHttpResponse.body.token).to.be.an('string');
-  });
-
-  it('Testando se não é possível fazer login sem o email', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({
-        password: 'senhaDeTesteCorreta'
-      });
-
-    expect(chaiHttpResponse.status).to.be.equal(400);
-    expect(chaiHttpResponse.body).to.be
-      .include({ 'message': 'All fields must be filled' }); 
-  });
-
-  it('Testando se não é possível fazer login sem o passsword', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({
-        email: 'teste@teste.com',
-      });
-
-      
       expect(chaiHttpResponse.status).to.be.equal(400);
       expect(chaiHttpResponse.body).to.be
         .include({ 'message': 'All fields must be filled' }); 
+    });
+
+    it('Testando se não é possível fazer login sem o passsword', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: 'admin@admin.com',
+        });
+
+        
+        expect(chaiHttpResponse.status).to.be.equal(400);
+        expect(chaiHttpResponse.body).to.be
+          .include({ 'message': 'All fields must be filled' }); 
+    });
+  });
+  describe('Passando dados incorretos', () => {
+    describe('Passando email incorreto', () => {
+      before(async () => {
+        sinon
+        .stub(user, "findOne")
+        .resolves(undefined);
+    });
+
+    after(()=>{ (user.findOne as sinon.SinonStub).restore() });
+
+    it('Testando se não é possível fazer login com um email inválido', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: 'admin@incorrect.com',
+          password: 'secret_admin'
+        });
+
+        expect(chaiHttpResponse.status).to.be.equal(401);
+        expect(chaiHttpResponse.body).to.be
+          .include({ 'message': 'Incorrect email or password' });  
+    });
   });
 
-  it('Testando se não é possível fazer login com um email inválido', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({
-        email: 'teste@.com',
-        password: 'senhaDeTesteCorreta'
+  describe('Passando password incorreto', () => {
+    before(async () => {
+      sinon
+        .stub(user, "findOne")
+        .resolves(loginSuccessful as user);
+    });
+
+    after(()=>{ (user.findOne as sinon.SinonStub).restore() });
+
+    it('Testando se não é possível fazer login com senha incorreta', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: 'admin@admin.com',
+          password: 'incorrect_secret_admin'
+        });
+
+        expect(chaiHttpResponse.status).to.be.equal(401);
+        expect(chaiHttpResponse.body).to.be
+          .include({ 'message': 'Incorrect email or password' });
       });
-
-      expect(chaiHttpResponse.status).to.be.equal(401);
-      expect(chaiHttpResponse.body).to.be
-      .include('Incorrect email or password');  
-  });
-
-  it('Testando se não é possível fazer login com senha incorreta', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({
-        email: 'teste@teste.com',
-        password: 'senhaDeTesteIncorreta'
-      });
-
-      expect(chaiHttpResponse.status).to.be.equal(401);
-      expect(chaiHttpResponse.body).to.be
-      .include('Incorrect email or password');
+    });
   });
 });
 
-describe('Rota /login/validate', () => {
-  let chaiHttpResponse: Response;
+// describe('Rota /login/validate', () => {
+//   let chaiHttpResponse: Response;
 
-  before(async () => {
-    sinon
-      .stub(user, "findOne")
-      .resolves(loginValidate as user);
-  });
+//   before(async () => {
+//     sinon
+//       .stub(user, "findOne")
+//       .resolves(loginValidate as user);
+//   });
 
-  after(()=>{
-    (user.findOne as sinon.SinonStub).restore();
-  });
+//   after(()=>{
+//     (user.findOne as sinon.SinonStub).restore();
+//   });
 
-  it('Testando se não é possível fazer requisição sem ter um token de autorização', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .get('/login/validate')
-      .set('Authorization', '');
+//   it('Testando se não é possível fazer requisição sem ter um token de autorização', async () => {
+//     chaiHttpResponse = await chai
+//       .request(app)
+//       .get('/login/validate')
+//       .set('Authorization', '');
 
-    expect(chaiHttpResponse.error.status).to.be.equal(401);
-    expect(chaiHttpResponse.error.message).to.be
-      .include('Unauthorized');
-  });
+//     expect(chaiHttpResponse.error.status).to.be.equal(401);
+//     expect(chaiHttpResponse.error.message).to.be
+//       .include('Unauthorized');
+//   });
 
-  it('Testando se não é possível fazer requisição sem ter um token de autorização', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .get('/login/validate')
-      .set('Authorization', 'token');
+//   it('Testando se não é possível fazer requisição sem ter um token de autorização', async () => {
+//     chaiHttpResponse = await chai
+//       .request(app)
+//       .get('/login/validate')
+//       .set('Authorization', 'token');
 
-    expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.an('object');
-    expect(chaiHttpResponse.body.role).to.be.true;
-  });
-});
+//     expect(chaiHttpResponse.status).to.be.equal(200);
+//     expect(chaiHttpResponse.body).to.be.an('object');
+//     expect(chaiHttpResponse.body.role).to.be.true;
+//   });
+// });
