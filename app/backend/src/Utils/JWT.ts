29a667efff40035366994cb4a42eb5036f893b62
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-29 21:24:50 -0300

    fix(JWT): :bug: Arrumando bugs de importação do arquivo JWT

--- app/backend/src/Utils/JWT.ts
+++ app/backend/src/Utils/JWT.ts
@@ -1,20 +1,20 @@
-import { SignOptions, verify, sign } from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 import IUSer from '../Interfaces/IUser';
 import ErrorHandle from '../Middleware/Class/error';
 
 const SECRET = process.env.JWT_SECRET || '96e2c84bf0244e7f8e4075b11ec81929';
 
-const jwtConfig: SignOptions = {
+const jwtConfig: jwt.SignOptions = {
   algorithm: 'HS256',
   noTimestamp: true,
 };
 
 export const generateJWTToken = (payload: Omit<IUSer, 'password'>) =>
-  sign(payload, SECRET, jwtConfig);
+  jwt.sign(payload, SECRET, jwtConfig);
 
 export const authenticateToken = async (token: string) => {
   try {
-    const hasValid = verify(token, SECRET, jwtConfig);
+    const hasValid = jwt.verify(token, SECRET, jwtConfig);
     return hasValid;
   } catch (_err) {
     throw new ErrorHandle(401, 'Invalid token');
