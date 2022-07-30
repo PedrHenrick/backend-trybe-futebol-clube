c7b5d1803fbe336c472caad40cc1844d6b059840
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-30 20:50:11 -0300

    feat(router): :sparkles: Adicionando rota /login/validate

--- app/backend/src/Routes/loginRouter.ts
+++ app/backend/src/Routes/loginRouter.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 import validateMiddleware from '../Middleware/validate.middleware';
 import loginSchema from '../Middleware/schemes';
 import LoginController from '../Controllers/Login.controller';
+import authenticateMiddleware from '../Middleware/auth.middleware';
 
 const loginRouter = Router();
 loginRouter.post('/', validateMiddleware(loginSchema), new LoginController().verifyLogin);
+loginRouter.get('/validate', authenticateMiddleware, new LoginController().validate);
 
 export default loginRouter;
