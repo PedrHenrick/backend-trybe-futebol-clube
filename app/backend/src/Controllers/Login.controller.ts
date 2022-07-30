93516e2e1c3003a5e887ad09eb1c1757015c5107
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-30 18:41:53 -0300

    fix(controller): :bug: Adicionando try/catch no controller para lidar com erros

--- app/backend/src/Controllers/Login.controller.ts
+++ app/backend/src/Controllers/Login.controller.ts
@@ -1,11 +1,15 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import ILogin from '../Interfaces/ILogin';
 import LoginService from '../Services/Login.service';
 
 class LoginController {
-  public verifyLogin = async (request: Request, response: Response) => {
-    const authResult = await new LoginService().authenticate(request.body as ILogin);
-    response.status(200).json({ token: authResult });
+  public verifyLogin = async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const authResult = await new LoginService().authenticate(request.body as ILogin);
+      response.status(200).json({ token: authResult });
+    } catch (err) {
+      next(err);
+    }
   };
 }
 
