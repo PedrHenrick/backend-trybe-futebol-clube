d986de0a786a39419df26d4cec8c5bc5bcc411a5
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-30 20:49:51 -0300

    perf(middleware): :zap: Editando middleware de erro após adicionar o express-async-errors

--- app/backend/src/Middleware/Error.middleware.ts
+++ app/backend/src/Middleware/Error.middleware.ts
@@ -1,10 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import ErrorHandle from './Class/error';
 
-const ErrorMiddleware = (err: Error, _req: Request, res: Response, next: NextFunction) => {
+const ErrorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
   const { status, message } = err as ErrorHandle;
   res.status(status).json({ message });
-  next();
 };
 
 export default ErrorMiddleware;
