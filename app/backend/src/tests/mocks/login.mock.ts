6e867699835844e51b7deed09d109fe8918ad2ce
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-30 18:45:00 -0300

    fix(mock): :bug: Alterando dados do mock utilizando os existentes nos testes oficiais

--- app/backend/src/tests/mocks/login.mock.ts
+++ app/backend/src/tests/mocks/login.mock.ts
@@ -1,10 +1,10 @@
 export const loginSuccessful = {
-  username: 'teste',
-  role: 'user',
-  email: 'teste@teste.com',
-  password: 'senhaDeTesteCorreta'
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
 }
 
 export const loginValidate = {
-  role: 'user',
+  role: 'admin',
 }
