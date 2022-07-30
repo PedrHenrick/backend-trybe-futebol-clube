a35c3036032e8fbd4814972562de9546de78423f
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-30 20:51:15 -0300

    feat(controller): :sparkles: editando função verifyLogin e adicionando função validate no controller da rota /login

--- app/backend/src/Controllers/Login.controller.ts
+++ app/backend/src/Controllers/Login.controller.ts
@@ -1,15 +1,18 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
+import { authenticateToken } from '../Utils/JWT';
 import ILogin from '../Interfaces/ILogin';
 import LoginService from '../Services/Login.service';
 
 class LoginController {
-  public verifyLogin = async (request: Request, response: Response, next: NextFunction) => {
-    try {
-      const authResult = await new LoginService().authenticate(request.body as ILogin);
-      response.status(200).json({ token: authResult });
-    } catch (err) {
-      next(err);
-    }
+  public verifyLogin = async (request: Request, response: Response) => {
+    const authResult = await new LoginService().authenticate(request.body as ILogin);
+    response.status(200).json({ token: authResult });
+  };
+
+  public validate = async (request: Request, response: Response) => {
+    const token = request.headers.authorization || '';
+    const userLogged = await authenticateToken(token);
+    response.status(200).json({ role: userLogged.role });
   };
 }
 
