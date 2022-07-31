16d6bc0605454f39c6278c0cd1e2d641b57c47e6
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-31 18:57:37 -0300

    feat(service): :sparkles: Criando função getOnTeam no service da rota /teams

--- app/backend/src/Services/Team.service.ts
+++ app/backend/src/Services/Team.service.ts
@@ -1,9 +1,15 @@
-// import ErrorHandle from '../Middleware/Class/error';
+import ErrorHandle from '../Middleware/Class/error';
 import TeamModel from '../Models/Team.model';
 
 export default class TeamService {
   public getAllTeams = async () => {
     const allTeams = await new TeamModel().getAllTeams();
     return allTeams;
+  };
+
+  public getOneTeam = async (id: number) => {
+    const team = await new TeamModel().getOneTeam(id);
+    if (!team) throw new ErrorHandle(400, 'User is not exists');
+    return team;
   };
 }
