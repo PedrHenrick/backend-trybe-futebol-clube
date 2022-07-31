aefb1bd7c666992fbe79020c4c59040addeef7fa
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-31 16:57:40 -0300

    fix: :bug: Corrigindo bug dos schemas

--- app/backend/src/Middleware/schemes.ts
+++ app/backend/src/Middleware/schemes.ts
@@ -4,7 +4,7 @@ const loginSchema = joi.object({
   email: joi.string().email().required(),
   password: joi.string().min(1).required(),
 }).messages({
-  'any.required': 'All fields must be filled',
+  '*': 'All fields must be filled',
 });
 
 export default loginSchema;
