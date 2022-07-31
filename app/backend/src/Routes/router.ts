e5eacfb3357162586efa80c3cde1a6648e2af6fa
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-31 19:51:54 -0300

    feat: :sparkles: Adicionando rota /matches mo router principal

--- app/backend/src/Routes/router.ts
+++ app/backend/src/Routes/router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import loginRouter from './loginRouter';
+import matchRouter from './matchRouter';
 import teamRouter from './teamRouter';
 
 const router = Router();
 
 router.use('/login', loginRouter);
 router.use('/teams', teamRouter);
+router.use('/matches', matchRouter);
 
 export default router;
