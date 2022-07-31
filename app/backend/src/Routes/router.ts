bdb21848247116f27b92e8649666643c765d3fd9
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-31 18:31:02 -0300

    feat(router): :sparkles: Adicionando rota /teams no router principal

--- app/backend/src/Routes/router.ts
+++ app/backend/src/Routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import loginRouter from './loginRouter';
+import teamRouter from './teamRouter';
 
 const router = Router();
 
 router.use('/login', loginRouter);
+router.use('/teams', teamRouter);
 
 export default router;
