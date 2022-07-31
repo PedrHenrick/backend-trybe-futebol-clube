d75965a3eb2e14328f5715776529c5a7bb02ead3
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-31 18:57:16 -0300

    feat(model): :sparkles: Criando função getOnTeam na model da rota /teams

--- app/backend/src/Models/Team.model.ts
+++ app/backend/src/Models/Team.model.ts
@@ -1,6 +1,7 @@
-// import ITeam from '../Interfaces/ITeam';
 import Team from '../database/models/team';
 
 export default class TeamModel {
   public getAllTeams = () => Team.findAll();
+
+  public getOneTeam = (id: number) => Team.findOne({ where: { id } });
 }
