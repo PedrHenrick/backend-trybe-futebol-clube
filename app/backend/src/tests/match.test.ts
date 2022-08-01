c899d3e846960abec4ed4d5759c2aff3f246cc4e
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-01 19:04:40 -0300

    test(test): :test_tube: Adicionand os testes do método post da rota /matches

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -5,9 +5,17 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import Match from '../database/models/match';
+import User from '../database/models/user';
 
 import { Response } from 'superagent';
-import { getMatchesInProgressFalseMock, getMatchesInProgressTrueMock, getMatchesMock } from './mocks/match.mock';
+import {
+  getMatchesInProgressFalseMock,
+  getMatchesInProgressTrueMock,
+  getMatchesMock,
+  postMatchMock,
+} from './mocks/match.mock';
+
+import { loginSuccessful } from './mocks/login.mock';
 
 chai.use(chaiHttp);
 
@@ -76,6 +84,162 @@ describe('Rota /Match', () => {
       expect(chaiHttpResponse.body[0].teamHome).to.be.exist;
       expect(chaiHttpResponse.body[0].teamAway).to.be.exist;
       expect(chaiHttpResponse.body).to.be.length(40);
+    });
+  });
+
+  describe('Adicionando partida', () => {
+    describe('Passando todos os dados corretamente', () => {
+      before(async () => {
+        sinon
+        .stub(Match, "create")
+        .resolves(postMatchMock as any);
+
+        sinon
+        .stub(User, "findOne")
+        .resolves(loginSuccessful as User);
+      });
+        
+  
+      after(async () => {
+        (Match.create as sinon.SinonStub).restore(),
+        (User.findOne as sinon.SinonStub).restore() 
+      });
+
+      it('Testando se é possível adicionar um usuário sem problemas', async () => {
+        chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+          email: 'admin@admin.com',
+          password: 'secret_admin'
+        });
+
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/matches')
+          .set('authorization', chaiHttpResponse.body.token)
+          .send({
+            homeTeam: 16,
+            awayTeam: 8,
+            homeTeamGoals: 2,
+            awayTeamGoals: 2
+          });
+        
+        expect(chaiHttpResponse.status).to.be.equal(201);
+        expect(chaiHttpResponse.body).to.be.an('object');
+        expect(chaiHttpResponse.body.id).to.be.exist;
+        expect(chaiHttpResponse.body.homeTeam).to.be.exist;
+        expect(chaiHttpResponse.body.awayTeam).to.be.exist;
+        expect(chaiHttpResponse.body.homeTeamGoals).to.be.exist;
+        expect(chaiHttpResponse.body.awayTeamGoals).to.be.exist;
+        expect(chaiHttpResponse.body.inProgress).to.be.exist;
+      });
+    });
+
+    describe('Faltando alguns dados', () => {
+      before(async () => {
+        sinon
+          .stub(User, "findOne")
+          .resolves(loginSuccessful as User);
+      });
+  
+      after(()=>{ (User.findOne as sinon.SinonStub).restore() });
+
+      it('Testando se não é possível adicionar um usuário sem o homeTeam', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+            email: 'admin@admin.com',
+            password: 'secret_admin'
+          });
+  
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/matches')
+          .set('authorization', chaiHttpResponse.body.token)
+          .send({
+            awayTeam: 8,
+            homeTeamGoals: 2,
+            awayTeamGoals: 2
+          });
+  
+        expect(chaiHttpResponse.status).to.be.equal(400);
+        expect(chaiHttpResponse.body).to.be
+          .include({ 'message': 'All fields must be filled' }); 
+      });
+  
+      it('Testando se não é possível adicionar um usuário sem o awayTeam', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+            email: 'admin@admin.com',
+            password: 'secret_admin'
+          });
+  
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/matches')
+          .set('authorization', chaiHttpResponse.body.token)
+          .send({
+            homeTeam: 16,
+            homeTeamGoals: 2,
+            awayTeamGoals: 2
+          });
+  
+        expect(chaiHttpResponse.status).to.be.equal(400);
+        expect(chaiHttpResponse.body).to.be
+          .include({ 'message': 'All fields must be filled' }); 
+      });
+  
+      it('Testando se não é possível adicionar um usuário sem o homeTeamGoals', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+            email: 'admin@admin.com',
+            password: 'secret_admin'
+          });
+  
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/matches')
+          .set('authorization', chaiHttpResponse.body.token)
+          .send({
+            homeTeam: 16,
+            awayTeam: 8,
+            awayTeamGoals: 2
+          });
+  
+        expect(chaiHttpResponse.status).to.be.equal(400);
+        expect(chaiHttpResponse.body).to.be
+          .include({ 'message': 'All fields must be filled' }); 
+      });
+  
+      it('Testando se não é possível adicionar um usuário sem o awayTeamGoals', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/login')
+          .send({
+            email: 'admin@admin.com',
+            password: 'secret_admin'
+          });
+
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/matches')
+          .set('authorization', chaiHttpResponse.body.token)
+          .send({
+            homeTeam: 16,
+            awayTeam: 8,
+            homeTeamGoals: 2,
+          });
+  
+        expect(chaiHttpResponse.status).to.be.equal(400);
+        expect(chaiHttpResponse.body).to.be
+          .include({ 'message': 'All fields must be filled' }); 
+      });
     });
   });
 });
