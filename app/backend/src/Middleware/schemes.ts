ad0ee3f0b8f7426336bd3c5bf050c09736d39d9c
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-01 01:57:34 -0300

    feat(middleware): :sparkles: Adicionando schema para rota post da rota /matches

--- app/backend/src/Middleware/schemes.ts
+++ app/backend/src/Middleware/schemes.ts
@@ -1,10 +1,15 @@
 import * as joi from 'joi';
 
-const loginSchema = joi.object({
+export const loginSchema = joi.object({
   email: joi.string().email().required(),
   password: joi.string().min(1).required(),
 }).messages({
   '*': 'All fields must be filled',
 });
 
-export default loginSchema;
+export const matchSchema = joi.object({
+  homeTeam: joi.number().min(1).required(),
+  awayTeam: joi.number().min(1).required(),
+  homeTeamGoals: joi.number().min(1).required(),
+  awayTeamGoals: joi.number().min(1).required(),
+});
