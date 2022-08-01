f98e39ef9e97bb5f60756da295674d99f0aa1ead
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-01 00:11:45 -0300

    feat(controller): :sparkles: Pegando os e verificando as queries para alterar a função chamada no controller

--- app/backend/src/Controllers/Match.controller.ts
+++ app/backend/src/Controllers/Match.controller.ts
@@ -2,7 +2,15 @@ import { Request, Response } from 'express';
 import MatchService from '../Services/Match.service';
 
 export default class MatchController {
-  public getAllMatches = async (_request: Request, response: Response): Promise<Response> => {
+  public getAllMatches = async (request: Request, response: Response): Promise<Response> => {
+    const { inProgress } = request.query;
+
+    if (inProgress) {
+      const matchesInProgress = await new MatchService()
+        .getMatchesInProgress(inProgress === 'true');
+      return response.status(200).json(matchesInProgress);
+    }
+
     const allMatches = await new MatchService().getAllMatches();
     return response.status(200).json(allMatches);
   };
