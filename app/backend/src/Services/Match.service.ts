b553ae25c28bcdd5a475f4fa7bdd04a45624f76b
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-01 01:58:30 -0300

    feat(service): :sparkles: Adicionando função addMatch no service da rota /matches

--- app/backend/src/Services/Match.service.ts
+++ app/backend/src/Services/Match.service.ts
@@ -1,4 +1,5 @@
 // import ErrorHandle from '../Middleware/Class/error';
+import IMatch from '../Interfaces/IMatch';
 import MatchModel from '../Models/Match.model';
 
 export default class MatchService {
@@ -10,5 +11,10 @@ export default class MatchService {
   public getMatchesInProgress = async (inProgress: boolean) => {
     const matchesInProgress = await new MatchModel().getMatchesInProgress(inProgress);
     return matchesInProgress;
+  };
+
+  public addMatch = async (additionScheme: IMatch) => {
+    const resultMatch = await new MatchModel().addMatch(additionScheme);
+    return resultMatch;
   };
 }
