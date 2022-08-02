a909112a05bf7ec6f145d23273b0037e391c32f1
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-02 20:24:56 -0300

    fix: :bug: Editando as regras de negócio do arquivo que gera as tabelas

--- app/backend/src/Utils/Leaderboard.ts
+++ app/backend/src/Utils/Leaderboard.ts
@@ -3,39 +3,73 @@ import IMatch from '../Interfaces/IMatch';
 import { ILeaderboards } from '../Interfaces/ILeaderboards';
 
 export default class Leaderboard {
-  public calculateWins = (matches: IMatch[]) => {
+  public calculateWins = (type: string, id: number, matches: IMatch[]) => {
     let wins = 0;
     matches.forEach((match) => {
-      if (match.homeTeamGoals > match.awayTeamGoals) wins += 1;
+      if (type === 'home') {
+        if (match.homeTeamGoals > match.awayTeamGoals) wins += 1;
+      } else if (type === 'away') {
+        if (match.awayTeamGoals > match.homeTeamGoals) wins += 1;
+      } else if (match.homeTeam === id) {
+        if (match.homeTeamGoals > match.awayTeamGoals) wins += 1;
+      } else if (match.awayTeamGoals > match.homeTeamGoals) wins += 1;
     });
     return wins;
   };
 
-  public calculateDraws = (matches: IMatch[]) => {
+  public calculateDraws = (type: string, id: number, matches: IMatch[]) => {
     let draws = 0;
     matches.forEach((match) => {
-      if (match.homeTeamGoals === match.awayTeamGoals) draws += 1;
+      if (type === 'home') {
+        if (match.homeTeamGoals === match.awayTeamGoals) draws += 1;
+      } else if (type === 'away') {
+        if (match.awayTeamGoals === match.homeTeamGoals) draws += 1;
+      } else if (match.homeTeam === id) {
+        if (match.homeTeamGoals === match.awayTeamGoals) draws += 1;
+      } else if (match.awayTeamGoals === match.homeTeamGoals) draws += 1;
     });
     return draws;
   };
 
-  public calculateLosses = (matches: IMatch[]) => {
+  public calculateLosses = (type: string, id: number, matches: IMatch[]) => {
     let losses = 0;
     matches.forEach((match) => {
-      if (match.homeTeamGoals < match.awayTeamGoals) losses += 1;
+      if (type === 'home') {
+        if (match.homeTeamGoals < match.awayTeamGoals) losses += 1;
+      } else if (type === 'away') {
+        if (match.awayTeamGoals < match.homeTeamGoals) losses += 1;
+      } else if (match.homeTeam === id) {
+        if (match.homeTeamGoals < match.awayTeamGoals) losses += 1;
+      } else if (match.awayTeamGoals < match.homeTeamGoals) losses += 1;
     });
     return losses;
   };
 
-  public calculateGoalsFavor = (matches: IMatch[]) => {
+  public calculateGoalsFavor = (type: string, id: number, matches: IMatch[]) => {
     let goals = 0;
-    matches.forEach((match) => { goals += match.homeTeamGoals; });
+    matches.forEach((match) => {
+      if (type === 'home') {
+        goals += match.homeTeamGoals;
+      } else if (type === 'away') {
+        goals += match.awayTeamGoals;
+      } else if (match.homeTeam === id) {
+        goals += match.homeTeamGoals;
+      } else if (match.awayTeam === id) goals += match.awayTeamGoals;
+    });
     return goals;
   };
 
-  public calculateGoalsOwn = (matches: IMatch[]) => {
+  public calculateGoalsOwn = (type: string, id: number, matches: IMatch[]) => {
     let goals = 0;
-    matches.forEach((match) => { goals += match.awayTeamGoals; });
+    matches.forEach((match) => {
+      if (type === 'home') {
+        goals += match.awayTeamGoals;
+      } else if (type === 'away') {
+        goals += match.homeTeamGoals;
+      } else if (match.homeTeam === id) {
+        goals += match.awayTeamGoals;
+      } else if (match.awayTeam === id) goals += match.homeTeamGoals;
+    });
     return goals;
   };
 
@@ -45,15 +79,15 @@ export default class Leaderboard {
       || b.goalsBalance - a.goalsBalance
       || b.goalsFavor - a.goalsFavor);
 
-  public leaderboard = async (team: ITeam, matches: IMatch[]) => {
+  public leaderboard = async (type: string, team: ITeam, matches: IMatch[]) => {
     const name = team.teamName;
     const totalGames = matches.length;
-    const totalVictories = this.calculateWins(matches);
-    const totalDraws = this.calculateDraws(matches);
+    const totalVictories = this.calculateWins(type, team.id, matches);
+    const totalDraws = this.calculateDraws(type, team.id, matches);
     const totalPoints = (totalVictories * 3) + totalDraws;
-    const totalLosses = this.calculateLosses(matches);
-    const goalsFavor = this.calculateGoalsFavor(matches);
-    const goalsOwn = this.calculateGoalsOwn(matches);
+    const totalLosses = this.calculateLosses(type, team.id, matches);
+    const goalsFavor = this.calculateGoalsFavor(type, team.id, matches);
+    const goalsOwn = this.calculateGoalsOwn(type, team.id, matches);
     const goalsBalance = goalsFavor - goalsOwn;
     const efficiency = Number(((totalPoints / (totalGames * 3)) * 100).toFixed(2));
 
